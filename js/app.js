2c116dca1f2deec755094e869aebc5dff390c2ab
Reinaldo Stephens <reinaldo9519@gmail.com>
2023-10-09 22:18:58 -0600

    agregando secciones

--- js/app.js
+++ js/app.js
@@ -8,3 +8,31 @@ function toogleMenu() {
 
 openMenuBtn.addEventListener("click", () => toogleMenu());
 closeMenuBtn.addEventListener("click", () => toogleMenu());
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            const id = entry.target.getAttribute("id");
+            const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+            if (entry.isIntersecting) {
+                document.querySelector(".menu a.selected").classList.remove("selected");
+                menuLink.classList.add("selected");
+            }
+        });
+    },
+    { rootMargin: "-30% 0px -70% 0px" }
+);
+
+menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", () => {
+        menu.classList.remove("menu-opened");
+    });
+
+    const hash = menuLink.getAttribute("href");
+    const target = document.querySelector(hash);
+    if (target) {
+        observer.observe(target);
+    }
+});
